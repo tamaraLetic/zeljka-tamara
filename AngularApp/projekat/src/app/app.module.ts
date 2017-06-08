7b38fce407f3327f83fb01a05be497d2f197ff7d
tamaraLetic <letictamara@yahoo.com>
2017-06-08 14:00:13 +0200

    Start registration

--- AngularApp/projekat/src/app/app.module.ts
+++ AngularApp/projekat/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CountryListComponent } from './country-list/country-list.component';
 import { CountryComponent } from './country/country.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountryListComponent,
-    CountryComponent
+    CountryComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
